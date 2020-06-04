280999f3436839091931140bf535ac8e224fe260
Park Juhyung <jh@codechain.io>
2020-06-04 17:02:42 +0900

    Use port in ping test

--- remote-trait-object-tests/src/lib.rs
+++ remote-trait-object-tests/src/lib.rs
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+mod connection;
 mod mod_main;
 mod mod_ping;
 #[cfg(test)]
