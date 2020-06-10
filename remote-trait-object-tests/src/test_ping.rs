248a2d7416a87a07ec381c5fc39fb5e1faa8ba24
Park Juhyung <jh@codechain.io>
2020-06-10 09:32:56 +0900

    Use log crate
    We can print logs in the test using the command below:
    `RUST_LOG=trace cargo test --all -- --nocapture`

--- remote-trait-object-tests/src/test_ping.rs
+++ remote-trait-object-tests/src/test_ping.rs
@@ -18,6 +18,10 @@ use super::mod_main::main_like as main_main;
 use super::mod_ping::main_like as ping_main;
 use crate::connection::{create_connection, ConnectionEnd};
 
+fn init_logger() {
+    let _ = env_logger::builder().is_test(true).try_init();
+}
+
 /// There are thee entities: commander, main module and ping module.
 /// Commander sends "start" message to the main module.
 /// If the main module receives "start" message, it sends "ping" to the ping module.
@@ -25,16 +29,15 @@ use crate::connection::{create_connection, ConnectionEnd};
 /// If the main module received "pong" response, send "pong received" to the commander.
 #[test]
 fn ping() {
-    // FIXME: use a logger
-    println!("ping test start");
+    init_logger();
+
+    debug!("ping test start");
     let (main_to_cmd, cmd_to_main) = create_connection();
     let (main_to_ping, ping_to_main) = create_connection();
 
-    // FIXME: use a logger
-    println!("Call main_main");
+    debug!("Call main_main");
     let _main_module = main_main(Vec::new(), main_to_cmd, main_to_ping);
-    // FIXME: use a logger
-    println!("Call ping_main");
+    debug!("Call ping_main");
     let _ping_module = ping_main(Vec::new(), ping_to_main);
 
     let ConnectionEnd {
@@ -42,13 +45,10 @@ fn ping() {
         receiver: from_main,
     } = cmd_to_main;
 
-    // FIXME: use a logger
-    println!("Send start cmd");
+    debug!("Send start cmd");
     to_main.send("request:start".to_string()).unwrap();
-    // FIXME: use a logger
-    println!("Recv pong response");
+    debug!("Recv pong response");
     let response = from_main.recv().unwrap();
     assert_eq!(response, "response:pong received".to_string());
-    // FIXME: use a logger
-    println!("Test finished");
+    debug!("Test finished");
 }
