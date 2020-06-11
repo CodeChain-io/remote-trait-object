368dedbcfc06f9fc388250c4cee21c3070a24b48
Park Juhyung <jh@codechain.io>
2020-06-11 11:28:29 +0900

    Make basesandbox Intra simple
    Currently remote trait object is not ready to use basesandbox. Make
    basesandbox simple for now until remote trait object became ready.
    
    * Use String instead of Vec<u8>
    * Do not use global variable
    * Make Ipc trait do not inherit IpcSend and IpcRecv
    * Add a simple new_both_ends function

--- basesandbox/src/ipc/intra.rs
+++ basesandbox/src/ipc/intra.rs
@@ -14,43 +14,19 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use super::*;
+use super::{Ipc, IpcRecv, IpcSend, RecvError, Terminate};
 use crossbeam::channel::{bounded, Receiver, Select, SelectTimeoutError, Sender};
-use once_cell::sync::OnceCell;
-use parking_lot::Mutex;
-use std::collections::hash_map::HashMap;
 
-struct RegisteredIpcEnds {
-    is_server: bool,
-    send: Sender<Vec<u8>>,
-    recv: Receiver<Vec<u8>>,
-    send_for_termination: Sender<()>,
-    recv_for_termination: Receiver<()>,
-}
-
-static POOL: OnceCell<Mutex<HashMap<String, RegisteredIpcEnds>>> = OnceCell::new();
-fn get_pool_raw() -> &'static Mutex<HashMap<String, RegisteredIpcEnds>> {
-    POOL.get_or_init(|| Mutex::new(HashMap::new()))
-}
-
-fn add_ends(key: String, ends: RegisteredIpcEnds) {
-    assert!(get_pool_raw().lock().insert(key, ends).is_none())
-}
-
-fn take_ends(key: &str) -> RegisteredIpcEnds {
-    get_pool_raw().lock().remove(key).unwrap()
-}
-
-pub struct IntraSend(Sender<Vec<u8>>);
+pub struct IntraSend(Sender<String>);
 
 impl IpcSend for IntraSend {
-    fn send(&self, data: &[u8]) {
-        self.0.send(data.to_vec()).unwrap()
+    fn send(&self, data: String) {
+        self.0.send(data).unwrap()
     }
 }
 
 pub struct IntraRecv {
-    data_receiver: Receiver<Vec<u8>>,
+    data_receiver: Receiver<String>,
     terminator_receiver: Receiver<()>,
     terminator: Sender<()>,
 }
@@ -68,7 +44,7 @@ impl Terminate for Terminator {
 impl IpcRecv for IntraRecv {
     type Terminator = Terminator;
 
-    fn recv(&self, timeout: Option<std::time::Duration>) -> Result<Vec<u8>, RecvError> {
+    fn recv(&self, timeout: Option<std::time::Duration>) -> Result<String, RecvError> {
         let mut selector = Select::new();
         let data_receiver_index = selector.recv(&self.data_receiver);
         let terminator_index = selector.recv(&self.terminator_receiver);
@@ -115,92 +91,35 @@ pub struct Intra {
     recv: IntraRecv,
 }
 
-impl IpcSend for Intra {
-    fn send(&self, data: &[u8]) {
-        self.send.send(data)
-    }
-}
-
-impl IpcRecv for Intra {
-    type Terminator = Terminator;
-
-    fn recv(&self, timeout: Option<std::time::Duration>) -> Result<Vec<u8>, RecvError> {
-        self.recv.recv(timeout)
-    }
-
-    fn create_terminator(&self) -> Self::Terminator {
-        self.recv.create_terminator()
-    }
-}
-
 impl Ipc for Intra {
-    fn arguments_for_both_ends() -> (Vec<u8>, Vec<u8>) {
-        let key_server = generate_random_name();
-        let key_client = generate_random_name();
-
-        let (send_server, recv_client) = bounded(256);
-        let (send_client, recv_server) = bounded(256);
-
-        {
-            let (send_for_termination, recv_for_termination) = bounded(1);
-            add_ends(key_server.clone(), RegisteredIpcEnds {
-                is_server: true,
-                send: send_server.clone(),
-                recv: recv_server,
-                send_for_termination,
-                recv_for_termination,
-            });
-        };
-        {
-            let (send_for_termination, recv_for_termination) = bounded(1);
-            add_ends(key_client.clone(), RegisteredIpcEnds {
-                is_server: false,
-                send: send_client,
-                recv: recv_client,
-                send_for_termination,
-                recv_for_termination,
-            });
-        }
-
-        (serde_cbor::to_vec(&key_server).unwrap(), serde_cbor::to_vec(&key_client).unwrap())
-    }
-
     type SendHalf = IntraSend;
     type RecvHalf = IntraRecv;
 
-    fn new(data: Vec<u8>) -> Self {
-        let key: String = serde_cbor::from_slice(&data).unwrap();
-        let RegisteredIpcEnds {
-            is_server,
-            send,
-            recv,
-            send_for_termination,
-            recv_for_termination,
-        } = take_ends(&key);
+    fn new_both_ends() -> (Self, Self) {
+        let (a_sender, a_receiver) = bounded(256);
+        let (a_termination_sender, a_termination_receiver) = bounded(1);
+        let (b_sender, b_receiver) = bounded(256);
+        let (b_termination_sender, b_termination_receiver) = bounded(1);
 
-        // Handshake
-        let timeout = std::time::Duration::from_millis(1000);
-        if is_server {
-            let x = recv.recv_timeout(timeout).unwrap();
-            assert_eq!(x, b"hey");
-            send.send(b"hello".to_vec()).unwrap();
-            let x = recv.recv_timeout(timeout).unwrap();
-            assert_eq!(x, b"hi");
-        } else {
-            send.send(b"hey".to_vec()).unwrap();
-            let x = recv.recv().unwrap();
-            assert_eq!(x, b"hello");
-            send.send(b"hi".to_vec()).unwrap();
-        }
-
-        Intra {
-            send: IntraSend(send),
+        let a_intra = Intra {
+            send: IntraSend(b_sender),
             recv: IntraRecv {
-                data_receiver: recv,
-                terminator: send_for_termination,
-                terminator_receiver: recv_for_termination,
+                data_receiver: a_receiver,
+                terminator_receiver: a_termination_receiver,
+                terminator: a_termination_sender,
             },
-        }
+        };
+
+        let b_intra = Intra {
+            send: IntraSend(a_sender),
+            recv: IntraRecv {
+                data_receiver: b_receiver,
+                terminator_receiver: b_termination_receiver,
+                terminator: b_termination_sender,
+            },
+        };
+
+        (a_intra, b_intra)
     }
 
     fn split(self) -> (Self::SendHalf, Self::RecvHalf) {
