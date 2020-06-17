5bdd77636cfff45099fbd811a436b258322cd35d
Park Juhyung <jh@codechain.io>
2020-06-17 16:17:44 +0900

    Add basesandbox executor
    I copied Junha's code.

--- basesandbox/src/execution.rs
+++ basesandbox/src/execution.rs
@@ -14,8 +14,5 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#[macro_use]
-extern crate log;
-
-pub mod execution;
-pub mod ipc;
+pub mod executee;
+pub mod executor;
