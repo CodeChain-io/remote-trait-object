a50bc494f4548025c94faf32e9bbf9d8b7cd98aa
Park Juhyung <jh@codechain.io>
2020-06-17 18:12:21 +0900

    Make test use multiple Ipc implementation

--- remote-trait-object-tests/src/test_ping.rs
+++ remote-trait-object-tests/src/test_ping.rs
@@ -21,7 +21,8 @@ use self::mod_main::main_like as main_main;
 use self::mod_ping::main_like as ping_main;
 use crate::connection::{create_connection, ConnectionEnd};
 use cbasesandbox::ipc::intra::Intra;
-use cbasesandbox::ipc::{IpcRecv, IpcSend};
+use cbasesandbox::ipc::unix_socket::DomainSocket;
+use cbasesandbox::ipc::{Ipc, IpcRecv, IpcSend};
 use remote_trait_object::{Packet, PacketView};
 use std::time::Duration;
 
@@ -29,18 +30,27 @@ fn init_logger() {
     let _ = env_logger::builder().is_test(true).try_init();
 }
 
+#[test]
+fn ping_intra() {
+    test_ping::<Intra>();
+}
+
+#[test]
+fn ping_unix_socket() {
+  test_ping::<DomainSocket>();
+}
+
 /// There are thee entities: commander, main module and ping module.
 /// Commander sends "start" message to the main module.
 /// If the main module receives "start" message, it sends "ping" to the ping module.
 /// If the ping module receives "ping" message, respond "pong".
 /// If the main module received "pong" response, send "pong received" to the commander.
-#[test]
-fn ping() {
+fn test_ping<IPC: Ipc + 'static>() {
     init_logger();
 
     debug!("ping test start");
-    let (main_to_cmd, cmd_to_main) = create_connection::<Intra>();
-    let (main_to_ping, ping_to_main) = create_connection::<Intra>();
+    let (main_to_cmd, cmd_to_main) = create_connection::<IPC>();
+    let (main_to_ping, ping_to_main) = create_connection::<IPC>();
 
     debug!("Call main_main");
     let _main_module = main_main(Vec::new(), main_to_cmd, main_to_ping);
