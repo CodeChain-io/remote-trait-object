8817734aaeeb3f62e6ecdf96eb49289acf2e1227
Park Juhyung <jh@codechain.io>
2020-06-17 10:40:16 +0900

    Make queue closeable

--- remote-trait-object/src/queue.rs
+++ remote-trait-object/src/queue.rs
@@ -14,35 +14,73 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use crossbeam::channel::{bounded, Receiver, Sender};
+use crossbeam::channel::{
+    bounded, Receiver,
+    RecvTimeoutError::{Disconnected, Timeout},
+    Sender,
+};
 use parking_lot::Mutex;
 
 /// Blocking concurrent Queue. (Crossbeam's queue doens't block)
 /// Please use the queue with Arc
 #[derive(Debug)]
 pub struct Queue<T> {
-    sender: Mutex<Sender<T>>,
-    recver: Mutex<Receiver<T>>,
+    // sender is None only when the close is called
+    sender: Mutex<Option<Sender<T>>>,
+    // receiver is None only when the close is called
+    recver: Mutex<Option<Receiver<T>>>,
 }
 
 impl<T> Queue<T> {
     pub fn new(size: usize) -> Self {
         let (sender, recver) = bounded(size);
         Queue {
-            sender: Mutex::new(sender),
-            recver: Mutex::new(recver),
+            sender: Mutex::new(Some(sender)),
+            recver: Mutex::new(Some(recver)),
         }
     }
 
-    pub fn push(&self, x: T) {
-        self.sender.lock().send(x).unwrap();
+    pub fn push(&self, x: T) -> Result<(), QueueClosed> {
+        let guard = self.sender.lock();
+        let sender = guard.as_ref().ok_or(QueueClosed)?;
+        sender.send(x).map_err(|_| QueueClosed)
     }
-
-    pub fn pop(&self, timeout: Option<std::time::Duration>) -> Result<T, ()> {
+    pub fn pop(&self, timeout: Option<std::time::Duration>) -> Result<T, PopError> {
+        let guard = self.recver.lock();
+        let recver = guard.as_ref().ok_or(PopError::QueueClosed)?;
         if let Some(duration) = timeout {
-            self.recver.lock().recv_timeout(duration).map_err(|_| ())
+            recver.recv_timeout(duration).map_err(|err| match err {
+                Timeout => PopError::Timeout,
+                Disconnected => PopError::QueueClosed,
+            })
         } else {
-            self.recver.lock().recv().map_err(|_| ())
+            recver.recv().map_err(|_| PopError::QueueClosed)
         }
     }
+
+    // FIXME: we need a better method to close channel
+    pub fn close(&self) {
+        // Try to get one of sender and recv.
+        // Since there is no case that both sender and recv are locked, we will eventually get one of the locks.
+        // If we drop one of them, the other part will drop automatically.
+        loop {
+            if let Some(mut guard) = self.recver.try_lock() {
+                guard.take().unwrap();
+                break
+            }
+            if let Some(mut guard) = self.sender.try_lock() {
+                guard.take().unwrap();
+                break
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct QueueClosed;
+
+#[derive(Debug)]
+pub enum PopError {
+    Timeout,
+    QueueClosed,
 }
