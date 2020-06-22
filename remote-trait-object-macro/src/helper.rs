55ffe6ce63f6d56002afa95a0886bf6693d8c37e
Junha Yang0 <junha@codechain.io>
2020-06-22 14:43:12 +0900

    Implement macro utility for path

--- remote-trait-object-macro/src/helper.rs
+++ remote-trait-object-macro/src/helper.rs
@@ -2,3 +2,17 @@ pub mod dispatcher;
 pub mod export_import;
 pub mod id;
 pub mod remote;
+
+pub fn path_of_single_ident(ident: syn::Ident) -> syn::Path {
+    syn::Path {
+        leading_colon: None,
+        segments: {
+            let mut punc = syn::punctuated::Punctuated::new();
+            punc.push(syn::PathSegment {
+                ident,
+                arguments: syn::PathArguments::None,
+            });
+            punc
+        },
+    }
+}
