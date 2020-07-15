4e18fd7f91c2efc9808c4f3eff15bb5e8c557102
Junha Yang0 <junha@codechain.io>
2020-07-15 17:37:52 +0900

    Add doc comments for the macro crate

--- remote-trait-object-macro/src/lib.rs
+++ remote-trait-object-macro/src/lib.rs
@@ -14,6 +14,9 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+//! This crate provides one core attribute procedural macro that is attached to the trait that you want to use as a service.
+//! See more details in `remote-trait-object` crate.
+
 #[macro_use]
 extern crate quote;
 
@@ -29,6 +32,8 @@ fn create_env_path() -> syn::Path {
     syn::parse2(quote! {remote_trait_object::macro_env}).unwrap()
 }
 
+/// It generates all necessary helper `struct`s that makes the trait be able to be used as a service.
+/// There will be many new public `struct`s, but you don't have to know about them.
 #[proc_macro_attribute]
 pub fn service(args: TokenStream, input: TokenStream) -> TokenStream {
     match service::service(TokenStream2::from(args), TokenStream2::from(input)) {
@@ -37,6 +42,7 @@ pub fn service(args: TokenStream, input: TokenStream) -> TokenStream {
     }
 }
 
+/// This macro consumes the target trait, and will print the expanded code. Use this when you want to see the result of macro.
 #[proc_macro_attribute]
 pub fn service_debug(args: TokenStream, input: TokenStream) -> TokenStream {
     match service::service(TokenStream2::from(args), TokenStream2::from(input)) {
