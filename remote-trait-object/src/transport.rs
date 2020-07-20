ef9d34aed279b6ca21d89f2c05f38e364e9e58d3
Junha Yang0 <junha@codechain.io>
2020-07-20 11:30:32 +0900

    Make multiplex pub(crate)

--- remote-trait-object/src/transport.rs
+++ remote-trait-object/src/transport.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-pub mod multiplex;
+pub(crate) mod multiplex;
 
 pub trait TransportSend: Send {
     /// It might block until counterparty's recv(). Even if not, the order is still guaranteed.
